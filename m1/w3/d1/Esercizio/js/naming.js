cc3d928c5527d9967663eaac63eb4b27eb6b9f53
beatricedaldin <beadaldin@gmail.com>
2022-07-05 08:21:37 +0200

    js

--- m1/w3/d1/Esercizio/js/naming.js
+++ m1/w3/d1/Esercizio/js/naming.js
@@ -14,3 +14,32 @@ var _pet2 = 'pantera';
 document.write(_pet2);
 
 //var 12 = 'numero';
+
+var pippo = 1
+var pluto = 2
+console.log(pippo);
+console.log(pluto);
+
+var nomeVariabile =  'Personal Computer'
+var nomeVariabile2 = '5'
+
+
+var listaDellaSpesa = [1, 2, 5, 'pane', 7, 'tappeto']
+var anagrafeCittadini  = {
+    nome: 'Emiliano',
+    eta: 30,
+    studente: true
+}
+console.table(anagrafeCittadini)
+console.log(listaDellaSpesa[3])
+console.log(listaDellaSpesa[0])
+
+var rigaDispari = document.getElementsByClassName('dispari')
+console.log(rigaDispari)
+
+rigaDispari.innerHTML =  'Purple'
+console.log(rigaDispari)
+
+rigaDispari[0].style.backgroundColor = 'purple';
+rigaDispari[1].style.backgroundColor = 'purple';
+rigaDispari[2].style.backgroundColor = 'purple';
