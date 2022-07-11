71714f226c86893e25af848f1f06c20d493c97ca
beatricedaldin <107913447+beatricedaldin@users.noreply.github.com>
2022-07-11 23:47:24 +0200

    riprova della radice quadrata

--- m1/w3/Progetto/assets/js/script.js
+++ m1/w3/Progetto/assets/js/script.js
@@ -32,7 +32,22 @@ function del(){
 
 }
 
-/* OVVIAMENTE NON FUNZIONA
+
+
+/* NOPE. QUESTO VA OLTRE LE MIE CAPACITà.
+DOVREBBE ESSERE UN IF DENTRO LA FUNZIONE RESULT()
+CHE FACCIA UN CHECK PER VEDERE SE ESISTE UN SIMBOLO √, A
+QUEL PUNTO ELIMINARLO, FARE LE OPERAZIONI MATEMATICHE NELLA 
+SEQUENZA DI DEFAULT E, POI, LA RADICE QUADRATA.
+IL PROBLEMA SORGE QUANDO DEVO FARE LA RADICE QUADRATA 
+DI UN SINGOLO NUMERO, IN UNA STRINGA PIENA DI OPERAZIONI
+MATEMATICHE. 
+ESISTE UN MODO CHE NON SIA MATH.SQRT() PER FARE LA RADICE?
+ESISTE UN SEMPLICE SIMBOLO RICONOSCIBILE DALLE OPERAZIONI DI 
+DEFAULT?
+MI ESPLODE IL CERVELLO.
+
+
 
 function root(){
     let display = document.querySelector('#display').value
@@ -41,5 +56,5 @@ function root(){
     let tot = eval(display.value)
     console.log(typeof display)
     console.log(display)
-    document.querySelector('#display').value += Math.sqrt(display)
+    document.querySelector('#display').value = Math.sqrt(tot)
 } */
