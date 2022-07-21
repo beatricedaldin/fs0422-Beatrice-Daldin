19956766d376d3b1f399db82edcf092c26b75d61
beatricedaldin <107913447+beatricedaldin@users.noreply.github.com>
2022-07-21 16:36:07 +0200

    eserciziomenudropdown

---
+++ m2/w1/d4/MENUDROPDOWN/assets/js/script.js
@@ -0,0 +1,9 @@
+let button = document.querySelector ("#aprimenubtn");
+
+
+button.addEventListener("click", function() {
+
+    let menu = document.getElementById("menu");
+    menu.classList.toggle("mostra");
+
+})
