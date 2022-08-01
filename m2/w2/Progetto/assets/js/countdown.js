7b204d20052a2738b4b27b4fa5c266f62d953053
beatricedaldin <107913447+beatricedaldin@users.noreply.github.com>
2022-08-01 04:20:23 +0200

    bastaaaaaa

--- m2/w2/Progetto/assets/js/countdown.js
+++ m2/w2/Progetto/assets/js/countdown.js
@@ -5,5 +5,5 @@ if (oggi.getMonth()==11 && oggi.getDate()>25)
 xmas.setFullYear(xmas.getFullYear()+1); 
 }  
 var ungiorno=1000*60*60*24;
-document.querySelector('#timer').innerHTML=(Math.ceil((xmas.getTime()-oggi.getTime())/(ungiorno))+
+document.querySelector('.timer').innerHTML=(Math.ceil((xmas.getTime()-oggi.getTime())/(ungiorno))+
 " giorni a Natale!");
