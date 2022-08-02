2d3e2335519bde27b05db035741393168fc48107
beatricedaldin <107913447+beatricedaldin@users.noreply.github.com>
2022-08-02 16:40:51 +0200

    kldfnsjdnf

---
+++ m2/w3/d2/script.js
@@ -0,0 +1,29 @@
+
+class Person{
+
+    constructor(nome, cognome, eta){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.eta = eta;
+
+    }
+    confrontoEta(){
+        if (this.eta > a.eta & this.eta > b.eta){
+            console.log ("il più vecchio è : " + this.nome)}
+              
+              else if(a.eta > this.eta & a.eta > b.eta){ 
+                      console.log ("il più vecchio è : " + a.nome)
+              }else if 
+          
+              (b.eta > this.eta & b.eta > a.eta) {
+              console.log ("il più vecchio è : " + b.nome)}
+    }
+}
+
+let p1 = ('Giulia', 'Chittaro', 30);
+let p2 = ('Mario', 'Rossi', 50);
+let p3 = ('Juan', 'Caracas', 23);
+
+
+p1.confrontaEtaUtenti( p2, p3);
+
