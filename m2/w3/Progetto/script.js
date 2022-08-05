d363a6e8f52b919af98aeb4149d094409c47eb16
beatricedaldin <107913447+beatricedaldin@users.noreply.github.com>
2022-08-05 15:23:18 +0200

    Progetto

---
+++ m2/w3/Progetto/script.js
@@ -0,0 +1,43 @@
+const apiUrl = 'users.json';
+
+fetch(apiUrl)
+.then(res => res.json())
+.then(res => {
+
+    let table = document.createElement('table');
+    let tHead = document.createElement('thead');
+    let tBody = document.createElement('tbody');
+    document.body.append(table);
+    table.append(tHead, tBody);
+
+    
+
+
+    for(let user of res){
+        
+        let lenghtUser = Object.keys(user).length;
+
+       
+        let userVal = Object.values(user);
+
+        let row = document.createElement('tr')
+        tBody.append(row)
+
+        for(i=0; i<lenghtUser; i++){
+            let td = document.createElement('td')
+            td.innerHTML = userVal[i]
+            row.append(td)
+        }
+
+       
+    }
+    
+    
+})
+    
+    
+    
+    
+        
+   
+
