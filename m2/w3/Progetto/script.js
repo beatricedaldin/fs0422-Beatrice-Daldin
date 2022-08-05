ab826606cc64e5aa2f5c1232eec5fc5cbb6e1ab5
beatricedaldin <107913447+beatricedaldin@users.noreply.github.com>
2022-08-05 17:00:22 +0200

    cheodio

--- m2/w3/Progetto/script.js
+++ m2/w3/Progetto/script.js
@@ -12,20 +12,22 @@ fetch(apiUrl)
 
     
 
+    
+
 
     for(let user of res){
-        
+        let userKeys = Object.keys(user)
         let lenghtUser = Object.keys(user).length;
-
-       
         let userVal = Object.values(user);
-
         let row = document.createElement('tr')
         tBody.append(row)
+        
 
         for(i=0; i<lenghtUser; i++){
+           
             let td = document.createElement('td')
             td.innerHTML = userVal[i]
+            th.innerHTML = userKeys[i]
             row.append(td)
         }
 
