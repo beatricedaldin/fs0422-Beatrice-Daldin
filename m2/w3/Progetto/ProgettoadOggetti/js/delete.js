eac11915c5f0486f0f19dc748bba96c70af578b5
beatricedaldin <107913447+beatricedaldin@users.noreply.github.com>
2022-08-08 01:47:48 +0200

    ultima

--- m2/w3/Progetto/ProgettoadOggetti/js/delete.js
+++ m2/w3/Progetto/ProgettoadOggetti/js/delete.js
@@ -1,39 +1,3 @@
-let goBackBtn = document.querySelector('#goBack')
-goBackBtn.addEventListener('click', function(){
-    location.href = 'index.html'
-})
-
-let deleteBtn = document.querySelector('#delete')
-let parametri = new URLSearchParams(location.search);
-let id = parametri.get('id')
-console.log(id)
-
-deleteBtn.addEventListener('click', function(id,utente){
-    const apiUtenti = 'http://localhost:3000/utenti';
-    
-    console.log(utente)
-    let options = {
-        method: 'DELETE',
-        headers:{
-            "content-type":"application/json"
-        }
-    }
-    
-    fetch(apiUtenti+'/'+id,options)
-    .then(res => res.json())
-    .then(res => {
-        utente.remove()
-        
-        Swal.fire({
-            position: 'top-end',
-            icon: 'success',
-            title: 'Utente Eliminato',
-            text:`L'utente è stato eliminato`,
-            showConfirmButton: false,
-            timer: 5000
-        })
-    })
-})
 
 
 
---
+++ m2/w3/Progetto/ProgettoadOggetti/js/profilo.js
@@ -0,0 +1,32 @@
+const apiUrl = 'http://localhost:3000/utenti';
+
+let params = new URLSearchParams(location.search);
+let userId = params.get('id')
+console.log(userId)
+
+fetch(apiUrl+'/'+userId)
+.then(res => res.json())
+.then(utente => {
+
+    let proUser = document.querySelector('#profileUsername')
+    proUser.innerHTML = utente.username
+    let firstName = document.querySelector('#firstName')
+    firstName.innerHTML = utente.firstName
+    let lastName = document.querySelector('#lastName')
+    lastName.innerHTML = utente.lastName
+    let gender = document.querySelector('#gender')
+    gender.innerHTML = utente.gender
+    let email = document.querySelector('#email')
+    email.innerHTML = utente.email
+    let house = document.querySelector('#house')
+    house.innerHTML = utente.house
+    let role = document.querySelector('#role')
+    role.innerHTML = utente.role
+    
+
+})
+
+let goBackBtn = document.querySelector('#goBack')
+goBackBtn.addEventListener('click', function(){
+    location.href='index.html'
+})
