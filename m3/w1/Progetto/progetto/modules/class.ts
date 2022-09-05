a0a28465aad5b4c98de883f8debe014ded193a31
beatricedaldin <107913447+beatricedaldin@users.noreply.github.com>
2022-09-05 04:26:17 +0200

    finito?

--- m3/w1/Progetto/progetto/modules/class.ts
+++ m3/w1/Progetto/progetto/modules/class.ts
@@ -1,53 +1,53 @@
 type lot = number | string | undefined
 
 interface ISmartphone {
-    carica:number
-    costoCallMinuto:number
-    costoSms:number
-    costoInternetSec:number
-    setInternet(secondi:number):void
-    setCarica(unaRicarica :number):void
-    setChiamata(minutiDurata:number):void
-    setMessaggio():void
-    numero404():number
+    carica: number
+    costoCallMinuto: number
+    costoSms: number
+    costoInternetSec: number
+    setInternet(secondi: number): void
+    setCarica(unaRicarica: number): void
+    setChiamata(minutiDurata: number): void
+    setMessaggio(): void
+    numero404(): number
 }
-abstract class Users{
-    nome : string | undefined
-    protected email : lot
-    constructor(nome : string | undefined, email : lot){
+abstract class Users {
+    nome: string | undefined
+    protected email: lot
+    constructor(nome: string | undefined, email: lot) {
         this.nome = nome
         this.email = email
     }
 }
 
-class User  extends Users implements ISmartphone{
+class User extends Users implements ISmartphone {
     carica: number
-    costoCallMinuto:number
-    costoSms:number
-    costoInternetSec:number
+    costoCallMinuto: number
+    costoSms: number
+    costoInternetSec: number
     private password: lot
-    constructor(carica: number, nome : string | undefined, email : lot, password : lot){
+    constructor(carica: number, nome: string | undefined, email: lot, password: lot) {
         super(nome, email)
         this.carica = carica
         this.costoCallMinuto = 0.20
         this.costoSms = 0.10
         this.costoInternetSec = 0.05
-        this.password = password 
+        this.password = password
     }
-    setCarica(unaRicarica : number){
-      this.carica +=unaRicarica
+    setCarica(unaRicarica: number) {
+        this.carica += unaRicarica
     }
-    setChiamata(minutiDurata: number){
+    setChiamata(minutiDurata: number) {
         let costoChiamata = this.costoCallMinuto * minutiDurata
         this.carica -= costoChiamata
     }
-    setMessaggio(){
+    setMessaggio() {
         this.carica -= this.costoSms
     }
     numero404(): number {
         return this.carica
     }
-    setInternet(secondi:number){
+    setInternet(secondi: number) {
         let costoInternet = this.costoInternetSec * secondi
         this.carica -= costoInternet
     }
@@ -55,8 +55,8 @@ class User  extends Users implements ISmartphone{
 
 class Contacts extends Users {
 
-    numTel:string | undefined
-    constructor(nome : string | undefined, email : lot, numTel:string | undefined){
+    numTel: string | undefined
+    constructor(nome: string | undefined, email: lot, numTel: string | undefined) {
         super(nome, email)
         this.numTel = numTel
     }
--- m3/w1/Progetto/progetto/script.ts
+++ m3/w1/Progetto/progetto/script.ts
@@ -14,7 +14,7 @@ function addZero(i: number | string) {
     return i;
 }
 
-setInterval (function() : any {
+setInterval(function (): any {
     let d: Date = new Date()
     let ora: string | number = addZero(d.getHours())
     let min: string | number = addZero(d.getMinutes())
@@ -27,8 +27,8 @@ setInterval (function() : any {
 
 
 powerBtn?.addEventListener('click', function (): void {
-        startScreen?.classList.add('active')
-        powerOnScreen?.classList.add('disabled')
+    startScreen?.classList.add('active')
+    powerOnScreen?.classList.add('disabled')
 })
 
 ///////////////////////////////////////////////
@@ -50,20 +50,20 @@ startBtn?.addEventListener('click', function (): void {
         let psw: lot = inptPsw?.value
         let nomeUser: string | undefined = inptNome?.value
         let email: lot = inptEmail?.value
-        let saldo : string | undefined = saldoSim?.value
-        let user : User = new User(Number(saldo),nomeUser, email, psw)
-        localStorage.setItem("user", JSON.stringify(user) || "") 
+        let saldo: string | undefined = saldoSim?.value
+        let user: User = new User(Number(saldo), nomeUser, email, psw)
+        localStorage.setItem("user", JSON.stringify(user) || "")
 
     }
 })
 
-let owner :object = JSON.parse(localStorage.getItem('user') || "")
-let user : User = new User(owner.carica, owner.nome, owner.email, owner.password);
-Object.assign(user , owner);
+let owner: object = JSON.parse(localStorage.getItem('user') || "")
+let user: User = new User(owner.carica, owner.nome, owner.email, owner.password);
+Object.assign(user, owner);
 console.log(user)
 
 //Con l'addeventlistener non funzionava
-function showPsw (){
+function showPsw() {
     if (inptPsw?.type === "password") {
         inptPsw.type = "text";
     } else {
@@ -71,7 +71,7 @@ function showPsw (){
     }
 }
 
-if(owner){
+if (owner) {
     mainScreen?.classList.add('active')
     powerOnScreen?.classList.add('disabled')
 }
@@ -83,7 +83,7 @@ let setBtn: HTMLButtonElement | null = document.querySelector('#settings')
 let setScreen: HTMLBodyElement | null = document.querySelector('#schermataSettings')
 let goBackBtns: HTMLCollectionOf<Element> = document.getElementsByClassName('goBack')
 let resetBtn: HTMLButtonElement | null = document.querySelector('#reset')
-let rechargeScreen : HTMLBodyElement | null = document.querySelector('#schermataRicarica')
+let rechargeScreen: HTMLBodyElement | null = document.querySelector('#schermataRicarica')
 
 
 setBtn?.addEventListener('click', function (): void {
@@ -91,10 +91,10 @@ setBtn?.addEventListener('click', function (): void {
     setScreen?.classList.add('active')
 })
 
-for( let goBackBtn of goBackBtns){
-goBackBtn?.addEventListener('click', function (): void {
-    location.reload()
-})
+for (let goBackBtn of goBackBtns) {
+    goBackBtn?.addEventListener('click', function (): void {
+        location.reload()
+    })
 }
 
 resetBtn?.addEventListener('click', function (): void {
@@ -124,36 +124,36 @@ creditoResiduo!.innerHTML = 'Il tuo credito residuo è di ' + user.carica + ' �
 //////////////////////////////////////////////////
 //////////RUBRICA&AGGIUNGICONTATTI
 
-let rubrica : HTMLButtonElement | null = document.querySelector('#rubrica')
-let addressScreen : HTMLBodyElement | null = document.querySelector('#schermataChiamate')
-let appendoCont : HTMLBodyElement | null = document.querySelector('#listaContatti')
-let addContacts : HTMLBodyElement | null = document.querySelector('#schermataRubrica')
-let calls : HTMLButtonElement | null = document.querySelector('#call')
+let rubrica: HTMLButtonElement | null = document.querySelector('#rubrica')
+let addressScreen: HTMLBodyElement | null = document.querySelector('#schermataChiamate')
+let appendoCont: HTMLBodyElement | null = document.querySelector('#listaContatti')
+let addContacts: HTMLBodyElement | null = document.querySelector('#schermataRubrica')
+let calls: HTMLButtonElement | null = document.querySelector('#call')
 
-rubrica?.addEventListener('click', function(): void{
+rubrica?.addEventListener('click', function (): void {
     addContacts?.classList.add('active')
     mainScreen?.classList.remove('active')
 })
-calls?.addEventListener('click', function(): void{
+calls?.addEventListener('click', function (): void {
     addressScreen?.classList.add('active')
     mainScreen?.classList.remove('active')
 })
 
-let contact1 : Contacts = new Contacts('Mamma', 'mamma@gmail.com', '123467890')
-let contact2 : Contacts = new Contacts('Marco', 'marcof@gmail.com', '0987654321')
-let contact3 : Contacts = new Contacts('Maria Lavoro', 'mariak@gmail.com', '6543210987')
+let contact1: Contacts = new Contacts('Mamma', 'mamma@gmail.com', '123467890')
+let contact2: Contacts = new Contacts('Marco', 'marcof@gmail.com', '0987654321')
+let contact3: Contacts = new Contacts('Maria Lavoro', 'mariak@gmail.com', '6543210987')
 
 let arr: object[] = []
 arr.push(contact1, contact2, contact3)
 
-for(let item of arr){
-    let post : HTMLDivElement = document.createElement('div')
+for (let item of arr) {
+    let post: HTMLDivElement = document.createElement('div')
     post.classList.add('post')
-    let p1 : HTMLParagraphElement = document.createElement('p')
+    let p1: HTMLParagraphElement = document.createElement('p')
     p1.classList.add('cNome')
-    let p2 : HTMLParagraphElement = document.createElement('p')
+    let p2: HTMLParagraphElement = document.createElement('p')
     p1.classList.add('cTel')
-    let callBtn : HTMLButtonElement = document.createElement('button')
+    let callBtn: HTMLButtonElement = document.createElement('button')
     callBtn.classList.add('callBtn')
     callBtn.innerHTML = `<i class="fa-solid fa-phone"></i>`
     p1.innerHTML = item.nome
@@ -194,47 +194,47 @@ add?.addEventListener('click', function():void{
 /////////////////////////////////////////////////////
 //CHIAMATE
 
-let callBtns : HTMLCollectionOf<Element> = document.getElementsByClassName('callBtn')
+let callBtns: HTMLCollectionOf<Element> = document.getElementsByClassName('callBtn')
 
-for( let btn of callBtns){
+for (let btn of callBtns) {
     btn?.addEventListener('click', function (): void {
-        if(user.carica>0){
-        let minsCall : number = Math.floor((Math.random()*60)-1)
-        user.setChiamata(minsCall)
-        owner.carica = parseFloat(user.carica.toFixed(2))
-        localStorage.setItem('user', JSON.stringify(owner))
-        Swal.fire({
-            position: 'center',
-            icon: 'success',
-            title: 'La tua chiamata di ' + minsCall + ' minuti ' + 'è terminata!',
-            showConfirmButton: false,
-            timer: 2500
-          })
-    } else {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Il tuo credito residuo non è sufficiente!'
-          })
-    }
+        if (user.carica > 0) {
+            let minsCall: number = Math.floor((Math.random() * 60) - 1)
+            user.setChiamata(minsCall)
+            owner.carica = parseFloat(user.carica.toFixed(2))
+            localStorage.setItem('user', JSON.stringify(owner))
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'La tua chiamata di ' + minsCall + ' minuti ' + 'è terminata!',
+                showConfirmButton: false,
+                timer: 2500
+            })
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Il tuo credito residuo non è sufficiente!'
+            })
+        }
     })
-    }
+}
 
 
 //////////////////////////////////////////////////
 ////////RICARICA
 
-let goToR : HTMLButtonElement | null = document.querySelector('#ricarica')
-let rechargeBtn : HTMLButtonElement | null = document.querySelector('#unaRicarica')
-let inptRecharge : HTMLInputElement | null = document.querySelector('#inptRicarica')
+let goToR: HTMLButtonElement | null = document.querySelector('#ricarica')
+let rechargeBtn: HTMLButtonElement | null = document.querySelector('#unaRicarica')
+let inptRecharge: HTMLInputElement | null = document.querySelector('#inptRicarica')
 
 
-goToR?.addEventListener('click', function(): void{
+goToR?.addEventListener('click', function (): void {
     mainScreen?.classList.remove('active')
     rechargeScreen?.classList.add('active')
 })
 
-rechargeBtn?.addEventListener('click', function(): void{
+rechargeBtn?.addEventListener('click', function (): void {
     user.setCarica(Math.floor(Number(inptRecharge?.value)))
     creditoResiduo!.innerHTML = 'Il tuo credito residuo è di ' + user.carica + ' €'
     inptRecharge!.value = ''
@@ -244,94 +244,96 @@ rechargeBtn?.addEventListener('click', function(): void{
         title: 'Hai effettuato una ricarica!',
         showConfirmButton: false,
         timer: 1500
-      })
-      owner.carica = user.carica
-      localStorage.setItem('user', JSON.stringify(owner))
-      console.log(owner)
+    })
+    owner.carica = user.carica
+    localStorage.setItem('user', JSON.stringify(owner))
+    console.log(owner)
 })
 
 ///////////////////////////////////////////////////
 //////////MESSAGGI
-let sms : HTMLButtonElement | null = document.querySelector('#sms')
-let textScreen : HTMLBodyElement | null = document.querySelector('#schermataMessaggi')
-let addText : HTMLInputElement | null = document.querySelector('#send')
-let chat : HTMLDivElement | null = document.querySelector('#him')
-let textMessage : HTMLInputElement | null = document.querySelector('#textMessage')
-let arrFrasi : string[] = ['Ciao!', 'Qui piove, da Te?', `Come è andato il colloquio?`, `Hai visto l'ultima puntata?`, 'Salve!', 'Buongiorno', 'Birretta?', 'Come stai?', `Com'è andata ieri sera?`, 'Sei viva?', 'Pizza?', `Stasera vai all'allenamento?`]  
-let randSent : HTMLCollectionOf<Element> = document.getElementsByClassName('sent')
+let sms: HTMLButtonElement | null = document.querySelector('#sms')
+let textScreen: HTMLBodyElement | null = document.querySelector('#schermataMessaggi')
+let addText: HTMLInputElement | null = document.querySelector('#send')
+let chat: HTMLDivElement | null = document.querySelector('#him')
+let textMessage: HTMLInputElement | null = document.querySelector('#textMessage')
+let arrFrasi: string[] = ['Ciao!', 'Qui piove, da Te?', `Come è andato il colloquio?`, `Hai visto l'ultima puntata?`, 'Salve!', 'Buongiorno', 'Birretta?', 'Come stai?', `Com'è andata ieri sera?`, 'Sei viva?', 'Pizza?', `Stasera vai all'allenamento?`]
+let randSent: HTMLCollectionOf<Element> = document.getElementsByClassName('sent')
 
-sms?.addEventListener('click', function(): void{
+sms?.addEventListener('click', function (): void {
     mainScreen?.classList.remove('active')
     textScreen?.classList.add('active')
-    for(let sent of randSent){
-        let randNumSent : number = Math.floor(Math.random()*(arrFrasi.length))
-        let sentence : string = arrFrasi[randNumSent]
+    for (let sent of randSent) {
+        let randNumSent: number = Math.floor(Math.random() * (arrFrasi.length))
+        let sentence: string = arrFrasi[randNumSent]
         sent.innerHTML = sentence
-        }
+    }
 })
 
-   
-        
 
-        addText?.addEventListener('click', function(): void{
-            if(user.carica>0){
-       
-        let chatRow : HTMLDivElement = document.createElement('div')
+
+
+addText?.addEventListener('click', function (): void {
+    if (user.carica > 0) {
+
+        let chatRow: HTMLDivElement = document.createElement('div')
         chatRow.classList.add('flexTextIcs')
-        let text : HTMLParagraphElement = document.createElement('p')
+        let text: HTMLParagraphElement = document.createElement('p')
         text.classList.add('text')
         text.innerHTML = textMessage!.value
-        let delX : HTMLParagraphElement = document.createElement('p')
+        let delX: HTMLParagraphElement = document.createElement('p')
         delX.classList.add('ics')
         delX.innerHTML = '+'
         chatRow.append(delX, text)
         chat?.append(chatRow)
         textMessage!.value = ''
 
-        delX.addEventListener('click', function():void{
+        delX.addEventListener('click', function (): void {
             chat?.removeChild(chatRow);
         })
 
-        
-            
-            user.setMessaggio()
-            owner.carica = parseFloat(user.carica.toFixed(2))
-            localStorage.setItem('user', JSON.stringify(owner))
-            
-        } else {
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Il tuo credito residuo non è sufficiente!'
-              })
-        }
+
+
+        user.setMessaggio()
+        owner.carica = parseFloat(user.carica.toFixed(2))
+        localStorage.setItem('user', JSON.stringify(owner))
+
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Il tuo credito residuo non è sufficiente!'
         })
+    }
+})
 
 
 //////////////////////////////////////////////////
 ////////INTERNET
 
-let intBtn : HTMLButtonElement | null = document.querySelector('#internet')
-let intScreen : HTMLBodyElement | null = document.querySelector('#schermataInternet')
+let intBtn: HTMLButtonElement | null = document.querySelector('#internet')
+let intScreen: HTMLBodyElement | null = document.querySelector('#schermataInternet')
 
-intBtn?.addEventListener('click', function():void{
+intBtn?.addEventListener('click', function (): void {
     timer()
     mainScreen?.classList.remove('active')
     intScreen?.classList.add('active')
 
     let secondi: number = 0
 
-    function timer ():void {setTimeout (function() {
-        secondi++
-        let secondis: HTMLBodyElement | null =document.querySelector('#secondis')
-        
-        user.carica--
-    }, 1000)}
-    
-    
+    function timer(): void {
+        setTimeout(function () {
+            secondi++
+            let secondis: HTMLBodyElement | null = document.querySelector('#secondis')
+
+            user.carica--
+        }, 1000)
+    }
+
+
     owner.carica = parseFloat(user.carica.toFixed(2))
     localStorage.setItem('user', JSON.stringify(owner))
-    
+
 })
 
 
@@ -339,7 +341,7 @@ intBtn?.addEventListener('click', function():void{
 
 
 
-    
+
 
 
 
