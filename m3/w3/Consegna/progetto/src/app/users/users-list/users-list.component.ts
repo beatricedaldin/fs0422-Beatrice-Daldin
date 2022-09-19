94b4e147446d56147efce84a7546805719c50c56
beatricedaldin <107913447+beatricedaldin@users.noreply.github.com>
2022-09-19 03:52:44 +0200

    erewrr

--- m3/w3/Consegna/progetto/src/app/users/users-list/users-list.component.ts
+++ m3/w3/Consegna/progetto/src/app/users/users-list/users-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
 import { UserService } from 'src/app/user.service';
 import { User } from '../user';
 
@@ -11,18 +12,32 @@ export class UsersListComponent implements OnInit {
 
   users:User[] = []
 
-  constructor(private userSvc:UserService) { }
+  constructor(private userSvc:UserService, private authSvc:AuthService) { 
+    
+  }
+
+  whoKnows: boolean = true
 
   ngOnInit(): void {
     this.userSvc.getAll().subscribe(users => this.users = users)
   }
+
+  visible(){
+    let userLogged: string[] = Object.values(this.authSvc.getLogged())
+    let userRole:string =  userLogged[2]
+    if(userRole=='admin'){
+      this.whoKnows = false
+    }
+  }
+
+  
 
   delete(id:number | undefined){
     
     this.userSvc.delete(id).subscribe(res => {
       let index = this.users.findIndex(u => u.id === id)
       this.users.splice(index,1)
-      
     })
   }
+  
 }
