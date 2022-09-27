c0146b733f3a6b87f20595a0460bc74ab005bf6e
beatricedaldin <107913447+beatricedaldin@users.noreply.github.com>
2022-09-27 16:25:26 +0200

    sadasdsa

--- backenddev/m1/w1/d2/consegna2709/src/consegna2709/Main3.java
+++ backenddev/m1/w1/d2/consegna2709/src/consegna2709/Main3.java
@@ -1,5 +1,7 @@
 package consegna2709;
 
+import java.util.Date;
+
 public class Main3 {
 
 	public static void main(String[] args) {
@@ -9,6 +11,39 @@ public class Main3 {
 
 }
 
-class Ecommerce{
+class Articolo{
+	int codArt;
+	String artName;
+	double price;
+	int artLeft;
+	
+	Articolo (int codArt, String artName, double price, int artLeft){
+		this.codArt = codArt;
+		this.artName = artName;
+		this.price = price;
+		this.artLeft = artLeft;
+	}
+}
+
+class Cliente{
+	int codCli;
+	String name;
+	String lastName;
+	String email;
+	Date regDate;
+	
+	Cliente(int codCli, String name, String lastName, String email, Date regDate){
+		this.codCli = codCli;
+		this.name = name;
+		this.lastName = lastName;
+		this.email = email;
+		this.regDate = regDate;
+	}
+
+}
+
+class Carrello{
+	int cliAss;
+	String[] articoli;
 	
 }
