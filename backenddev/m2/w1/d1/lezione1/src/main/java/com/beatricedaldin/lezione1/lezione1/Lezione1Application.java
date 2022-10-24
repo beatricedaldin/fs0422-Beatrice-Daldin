50c010d95a56d9a821f944c2c50f8a3d4f66edb8
beatricedaldin <107913447+beatricedaldin@users.noreply.github.com>
2022-10-24 16:31:52 +0200

    spring

---
+++ backenddev/m2/w1/d1/lezione1/src/main/java/com/beatricedaldin/lezione1/lezione1/Lezione1Application.java
@@ -0,0 +1,14 @@
+package com.beatricedaldin.lezione1.lezione1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lezione1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lezione1Application.class, args);
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ backenddev/m2/w1/d1/lezione1/src/test/java/com/beatricedaldin/lezione1/lezione1/Lezione1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beatricedaldin.lezione1.lezione1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lezione1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
